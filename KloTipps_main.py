bbd620ec640be3af4a106ae1e25108011d914e10
Schwannes <johannesbrinz@web.de>
2021-09-03 12:32:22 +0200

    Version_03.09.2021

--- KloTipps_main.py
+++ KloTipps_main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
 
 class MainWindow(Screen):
     pass
@@ -8,13 +9,32 @@ class MainWindow(Screen):
 class SecondWindow(Screen):
     pass
 
+class CreateAccountWindow(Screen):
+
+    namee = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def reset(self):
+        self.password.text = ""
+        self.namee.text = ""
+
+    def login(self):
+        self.reset()
+        sm.current = "main"
+
+
 class WindowManager(ScreenManager):
     pass
 
 
-
+sm = WindowManager()
 kv = Builder.load_file("my.kv")
 
+screens = [SecondWindow(name="second"), CreateAccountWindow(name="create"),MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+    
 class MyMainApp(App):
     def build(self):
         return kv
